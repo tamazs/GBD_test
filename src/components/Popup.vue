cd4bf382bc6297c2b3d9ac6849e343fb15f94c6b
Tamás Major <majortamas313@gmail.com>
2024-01-08 18:47:01 +0100

    popup scss

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -64,111 +64,103 @@ const sendRequest = () => {
 };
 </script>
   
-<style scoped>
+<style lang="scss" scoped>
 .popup-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: rgba(0, 0, 0, 0.4);
-}
-
-.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(0, 0, 0, 0.4);
+  
+  .popup {
     background: white;
     padding: 2rem;
     border-radius: 10px;
     width: 80%;
     max-width: 500px;
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
+    
+    h2 {
+      text-align: center;
+      font-size: 2.3rem;
+      font-weight: normal;
+    }
 
-.popup h2 {
-    text-align: center;
-    font-size: 2.3rem;
-    font-weight: normal;
-}
+    .feature-list, 
+    .feature-request label {
+      font-weight: bold;
+      margin: 2rem 0rem;
 
-.feature-list {
-    margin: 2rem 0rem;
-}
+      p {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+      }
+    }
 
-.feature-list p,
-.feature-request label {
-    font-weight: bold;
-}
+    .feature-request {
+      input {
+        width: 100%;
+        padding: 0.5rem;
+        margin-top: 0.5rem;
+        margin-bottom: 1rem;
+      }
+    }
 
-.feature-list p {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-}
+    .buttons {
+      display: flex;
+      justify-content: space-between;
+      gap: 1rem;
 
-.feature-request input {
-    width: 100%;
-    padding: 0.5rem;
-    margin-top: 0.5rem;
-    margin-bottom: 1rem;
-}
+      button {
+        padding: 0.7rem 1rem;
+        border: none;
+        border-radius: 12px;
+        cursor: pointer;
+        width: 50%;
+        font-size: 1rem;
 
-.buttons {
-    display: flex;
-    justify-content: space-between;
-    gap: 1rem;
-}
+        &:first-child {
+          background: #E9EFF2;
+        }
 
-.buttons button {
-    padding: 0.7rem 1rem;
-    border: none;
-    border-radius: 12px;
-    cursor: pointer;
-    width: 50%;
-    font-size: 1rem;
-}
+        &:last-child {
+          background: #3D22CF;
+          color: white;
+        }
 
-.buttons button:first-child {
-    background: #E9EFF2;
-}
+        &.disabled {
+          background: #ccc;
+          cursor: not-allowed;
+        }
+      }
+    }
 
-.buttons button:last-child {
-    background: #3D22CF;
-    color: white;
-}
-
-.buttons button.disabled {
-    background: #ccc;
-    cursor: not-allowed;
-}
-
-i {
-    color: #9AA7AF;
-}
-
-label {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+    i.fas.fa-circle-info {
+      color: #9AA7AF;
+    }
+  }
 }
 
 .tooltip {
-    position: absolute;
-    padding: 1rem;
-    background: rgb(231, 231, 231);
-    color: black;
-    border-radius: 4px;
-    z-index: 100;
-    display: flex;
-    align-items: center;
-    font-weight: normal;
-    font-size: 1rem;
-}
-
-.tooltip i {
+  position: absolute;
+  padding: 1rem;
+  background: rgb(231, 231, 231);
+  color: black;
+  border-radius: 4px;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  font-weight: normal;
+  font-size: 1rem;
+  
+  i {
     margin-right: 0.5rem;
     font-size: 1.5rem;
+  }
 }
 </style>
-  
